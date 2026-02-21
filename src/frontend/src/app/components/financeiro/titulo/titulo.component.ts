f4085f5166479fa901e673bfbaf5cc083e0b13b4
Tiago J Pereira <pereira.tiagojoao@gmail.com>
2026-02-21 17:52:54 -0300

    add navigate titulo to movimentacao

--- src/frontend/src/app/components/financeiro/titulo/titulo.component.ts
+++ src/frontend/src/app/components/financeiro/titulo/titulo.component.ts
@@ -1,27 +1,41 @@
 import { Component } from '@angular/core';
 
-import { ViewMode } from '../../base/model/view-mode.enum';
 import { TituloDetalheComponent } from './titulo-detalhe/titulo-detalhe.component';
 import { TituloGridComponent } from './titulo-grid/titulo-grid.component';
+import { TituloGridDTO } from './model/titulo-grid-dto';
+import { MovimentacaoFinanceiraDetalheComponent } from '../movimentacao-financeira/movimentacao-financeira-detalhe/movimentacao-financeira-detalhe.component';
 
 @Component({
   selector: 'gi-titulo',
-  imports: [TituloDetalheComponent, TituloGridComponent],
+  imports: [
+    TituloDetalheComponent,
+    TituloGridComponent,
+    MovimentacaoFinanceiraDetalheComponent,
+  ],
   providers: [],
   templateUrl: './titulo.component.html',
   styleUrl: './titulo.component.css',
 })
 export class TituloComponent {
-  viewMode: ViewMode = ViewMode.GRID;
+  viewMode: 'GRID' | 'DETAIL' | 'MOVIMENTACAO' = 'GRID';
   detailId: string | number = 'add';
+  titulosSelecionados: TituloGridDTO[] = [];
 
   toggleView() {
-    this.viewMode =
-      this.viewMode === ViewMode.GRID ? ViewMode.DETAIL : ViewMode.GRID;
+    this.viewMode = this.viewMode === 'GRID' ? 'DETAIL' : 'GRID';
   }
 
   openDetail($event: string | number) {
     this.detailId = $event;
     this.toggleView();
+  }
+
+  openMovimentacao(titulos: TituloGridDTO[]) {
+    this.titulosSelecionados = titulos;
+    this.viewMode = 'MOVIMENTACAO';
+  }
+
+  backToGrid() {
+    this.viewMode = 'GRID';
   }
 }
